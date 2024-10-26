9f8706ff8d2deb2ecb048df25910bc04d4af8809
James Shipley <james0014.shipley@gmail.com>
2024-10-26 18:03:05 +0200

    tmp

---
+++ mjprofiler/pure_simulation_benchmark.py
@@ -0,0 +1,69 @@
+import datetime
+import multiprocessing
+import time
+from concurrent.futures.process import ProcessPoolExecutor as ProcPool
+import mujoco
+import numpy as np
+
+from mjprofiler.bodies import Ant, Humanoid
+
+POPULATION_SIZE = (100, 200, 400, 800, 1600, 3200)
+N_STEPS = (100, 200, 400, 800, 1600, 3200)
+BODIES = (Ant.make(10, 10, 10), Humanoid.make(10))
+SIN = np.sin(np.arange(0, 2 * np.pi, 0.01))
+N_PROCS = 16
+
+
+def _run(model, n_steps, i_population):
+    _t = time.perf_counter()
+
+    data = mujoco.MjData(model)
+
+    for i_step in range(n_steps):
+        mujoco.mj_step(model, data)
+        data.ctrl = SIN[(i_population + i_step) % len(SIN)]
+    return time.perf_counter() - _t
+
+
+def _cpu_sim_single(population: int, n_steps: int, body_xml: str, attempts: int) -> float:
+    print(f"{population=} | {n_steps=}")
+    model = mujoco.MjModel.from_xml_string(body_xml)
+
+    times = []
+    with ProcPool(max_workers=N_PROCS) as pool:
+        for _ in range(attempts):
+            t = time.perf_counter()
+            if n_steps > 200:
+                futures = [pool.submit(_run, model, n_steps, i_population) for i_population in range(population)]
+                t2 = sum([fut.result() for fut in futures])
+            else:
+                t2 = sum(_run(model, n_steps, i_population) for i_population in range(population))
+            times.append(time.perf_counter() - t)
+            print(f'util: {t2 / times[-1]}, n_procs: {N_PROCS} t:{times[-1]}')
+
+    return sum(times) / len(times)
+
+
+def main_cpu(body_xml: str, attempts: int):
+    _cpu_count = multiprocessing.cpu_count()
+    print(f"started {datetime.datetime.now()} with {_cpu_count} processes")
+
+    x, y = len(POPULATION_SIZE), len(N_STEPS)
+
+    results = np.zeros((x, y))
+
+    for i_population in range(x):
+        for i_steps in range(y):
+            results[i_population, i_steps] = _cpu_sim_single(
+                    POPULATION_SIZE[i_population],
+                    N_STEPS[i_steps],
+                    body_xml,
+                    attempts
+                )
+
+    print(results)
+    print(sum(results.reshape(-1)))
+
+
+if __name__ == '__main__':
+    main_cpu(BODIES[0], 1)
